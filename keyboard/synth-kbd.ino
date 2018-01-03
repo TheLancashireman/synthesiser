a4938149d44c9d294e1f9f4ff93f5010da673c2f
David Haworth <dh@thelancashireman.org>
2018-01-04 00:09:25 +0100

    Keyboard now sends midi codes.

--- keyboard/synth-kbd.ino
+++ keyboard/synth-kbd.ino
@@ -33,29 +33,32 @@
 
 #define led1		13		// On-board LED connected to digital pin 13
 
-// Pins for scan lines
-#define scan_0		2		// PD2
-#define scan_1		3		// PD3
-#define scan_2		4		// PD4
+// Pins for data lines
+#define data_out_0	2		// PD2		}
+#define data_out_1	3		// PD3		} Column selectors for scan --> A0..A2 of 74HCT138
+#define data_out_2	4		// PD4		}
 
-// Pins for return lines
-#define rtn_0		5		// PD5
-#define rtn_1		6		// PD6
-#define rtn_2		7		// PD7
-#define rtn_3		8		// PB0
-#define rtn_4		9		// PB1
-#define rtn_5		10		// PB2
-#define rtn_6		11		// PB3
-#define rtn_7		12		// PB4
+#define data_out_3	5		// PD5		}
+#define data_out_4	6		// PD6		} Row selectors for scan --> S0..S2 of 74HCT354
+#define data_out_5	7		// PD7		}
+
+#define data_out_6	8		// PD7
+#define data_out_7	9		// PD7
+
+#define kscan_en	11		// PB3 - enables the Y output of the '354  (OE3)
+#define data_in		12		// PB4 - 
 
 #define kbd_nscan	6		/* No. of scan lines */
 #define kbd_nrtn	8		/* No. of return lines */
 
-const unsigned char scanline[3] =
-{	scan_0, scan_1, scan_2	};
+const unsigned char dataline[8] =
+{	data_out_0, data_out_1, data_out_2, data_out_3, data_out_4, data_out_5, data_out_6, data_out_7 };
 
-const unsigned char rtnline[kbd_nrtn] =
-{	rtn_0, rtn_1, rtn_2, rtn_3, rtn_4, rtn_5, rtn_6, rtn_7	};
+const unsigned char scanline[3] =
+{	data_out_0, data_out_1, data_out_2	};
+
+const unsigned char rtnline[3] =
+{	data_out_3, data_out_4, data_out_5	};
 
 struct keystate_s
 {
@@ -64,6 +67,34 @@ struct keystate_s
 };
 
 keystate_s keystate[64];
+
+/* Keymap.
+ * Numbers in the range 0..127 are midi notes.
+ * Numbers in the range 0x80-0xfe are switches.
+ * 0xff means do nothing.
+ * The columns (x0..x7) and rows (Ax..Hx) of this table are the raw alphanumeric scan codes in the test version.
+*/
+const unsigned char keymap[64] =
+{//	x0		x1		x2		x3		x4		x5		x6		x7
+	45,		46,		47,		48,		41,		42,		43,		44,		// Ax
+	37,		38,		39,		40,		33,		34,		35,		36,		// Bx
+	29,		30,		31,		32,		25,		26,		27,		28,		// Cx
+	5,		6,		7,		8,		0xff,	0xff,	0xff,	0xff,	// Dx
+	13,		14,		15,		16,		9,		10,		11,		12,		// Ex
+	21,		22,		23,		24,		17,		18,		19,		20,		// Fx
+	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	// Gx
+	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	0xff,	// Hx
+};
+
+char midi_chan = 0;
+char midi_offset = 36;
+
+void midi_send(char p0, char p1, char p2)
+{
+	Serial.print(p0);
+	Serial.print(p1);
+	Serial.print(p2);
+}
 
 /* setup() - standard Arduino "Init Task"
 */
@@ -74,13 +105,16 @@ void setup(void)
 	pinMode(led1, OUTPUT);				// Sets the digital pin as output
 	digitalWrite(led1, LOW);			// Drive the pin low (LED off)
 
-	for ( i = 0; i < 3; i++ )
+	pinMode(kscan_en, OUTPUT);			// Switch data from keyscan onto input pin.
+	digitalWrite(kscan_en, LOW);		//  -- disable.
+
+	pinMode(data_in, INPUT_PULLUP);		// Data input pin
+
+	for ( i = 0; i < 8; i++ )			// 8 data outputs
 	{
-		pinMode(scanline[i], OUTPUT);
+		pinMode(dataline[i], OUTPUT);
 		digitalWrite(scanline[i], HIGH);
 	}
-	for ( i = 0; i < kbd_nrtn; i++ )
-		pinMode(rtnline[i], INPUT_PULLUP);
 
 	for ( i = 0; i < 64; i++ )
 	{
@@ -88,16 +122,16 @@ void setup(void)
 		keystate[i].count = 0;
 	}
 
-	Serial.begin(9600);					// Start the serial port.
+	Serial.begin(115200);					// Start the serial port.
+#if 0
 	Serial.println("Hello world");
+#endif
 }
 
 /* loop() - standard Arduino "Background Task"
 */
 void loop(void)
 {
-	unsigned scan = 7;
-	unsigned rtn = 7;
 	int ledCount = 100;
 	int ledState = 0;
 	unsigned long then;
@@ -148,6 +182,8 @@ void loop(void)
 		{
 			unsigned scan, rtn, npress = 0;
 
+			digitalWrite(kscan_en, HIGH);		// Enable selected scan data onto input pin
+
 			for ( scan = 0; scan < kbd_nscan; scan++ )
 			{
 				digitalWrite(scanline[0], (scan & 0x01) == 0 ? LOW : HIGH);
@@ -156,12 +192,17 @@ void loop(void)
 
 				for ( rtn = 0; rtn < kbd_nrtn; rtn++ )
 				{
+					digitalWrite(rtnline[0], (rtn & 0x01) == 0 ? LOW : HIGH);
+					digitalWrite(rtnline[1], (rtn & 0x02) == 0 ? LOW : HIGH);
+					digitalWrite(rtnline[2], (rtn & 0x04) == 0 ? LOW : HIGH);
+
 					unsigned idx = scan*8 + rtn;
+					int k;
 
 					if ( keystate[idx].count > 0 )
 						keystate[idx].count--;			// Ignore keys whose count is non-zero
 					else
-					if ( digitalRead(rtnline[rtn]) == LOW )
+					if ( digitalRead(data_in) == LOW )
 					{
 						// Key down
 						if ( keystate[idx].state == 0 )
@@ -170,12 +211,23 @@ void loop(void)
 							keystate[idx].state = 1;
 							keystate[idx].count = 2;		// 20 ms
 
+#if 1
+							if ( (k = keymap[idx]) <= 127 )
+							{
+								k += midi_offset;
+								if ( k >= 0 && k <= 127 )
+								{
+									midi_send(0x90+midi_chan, k, 127);
+								}
+							}
+#else
 							npress++;
 							char c = 'A'+scan;
 							Serial.print(c);
 							c = '0'+rtn;
 							Serial.print(c);
 							Serial.print(' ');
+#endif
 						}
 					}
 					else
@@ -186,17 +238,30 @@ void loop(void)
 							// Released
 							keystate[idx].state = 0;
 							keystate[idx].count = 2;		// 20 ms
-
+#if 1
+							if ( (k = keymap[idx]) <= 127 )
+							{
+								k += midi_offset;
+								if ( k >= 0 && k <= 127 )
+								{
+									midi_send(0x80+midi_chan, k, 127);
+								}
+							}
+#else
 							npress++;
 							char c = 'a'+scan;
 							Serial.print(c);
 							c = '0'+rtn;
 							Serial.print(c);
 							Serial.print(' ');
+#endif
 						}
 					}
 				}
 			}
+
+			digitalWrite(kscan_en, LOW);		// Disable selected scan data (high impedence)
+
 			if ( npress > 0 )
 				Serial.println('!');
 		}
