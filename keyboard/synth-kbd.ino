ee72d5fa741c9d9ea288d6768f4c2ccfbef0e6a8
David Haworth <dh@thelancashireman.org>
2018-04-08 21:58:14 +0200

    Fix a copy-pasta error in the GPL header.

--- keyboard/synth-kbd.ino
+++ keyboard/synth-kbd.ino
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DhG.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  *
  * $Id: synth-kbd.ino 3 2016-11-24 04:25:43Z dave $
  *
