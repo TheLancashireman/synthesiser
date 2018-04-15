b2fd56cccaf77b7d83088cb0aeae64e8eee213d1
David Haworth <dh@thelancashireman.org>
2018-04-15 18:36:05 +0200

    Add a standalone HOST_TEST option. Fix the calculation: we don't use FP as loop counters or increments, do we?

--- organ/waveform.c
+++ organ/waveform.c
@@ -1,44 +1,100 @@
 /* waveform.c
  *
- * Simple waveform generator
+ * Simple sine-wave generator.
+ *
+ * The function waveform() generates a sine wave represented as a sequence of 24-bit integers.
+ * One cycle of the sine wave is generated. This means that there will be a bit of distortion
+ * caused by the rounding of the number of samples in the wavelength. If this distortion is
+ * audible, a solution might be to generate waveforms or two or more cycles to reduce the error.
+ *
+ * (c) David Haworth
+ *
+ * License: GPL 3.0
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <organ.h>
+#include <math.h>
 
-const double sample_freq = 48000.0;
-const double scale = 32768.0;
+#ifndef HOST_TEST
+#define HOST_TEST 0
+#endif
+
+#if HOST_TEST
+#include <stdio.h>
+#endif
+
+const double sample_freq = 48000.0;		/* 48 kHz */
+const double scale = 8388608.0;			/* 24 bits */
 const double pi = 3.14159265;
 
-void waveform(tg_sinewave_t *wave, double sine_freq)
+/* Calculate the number of samples in a single cycle of a given frequency.
+*/
+int wavelen(double sine_freq)
 {
-	double angle_step = (2.0 * pi * sine_freq) / sample_freq;
-	double angle = 0.0;
-	double amplitude = 1.0;
-	int nsamples = (int)( sample_freq / sine_freq + 0.5 );
-	int sample = 0;
-
-	fprintf(stderr, "Wavform for %fHz ; nsamples = %d\n", sine_freq, nsamples);
-
-	/* Allocate space for the waveform.
-	*/
-	wave->wave = (tg_amplitude_t *)calloc(nsamples, sizeof(tg_amplitude_t));
-	if ( wave->wave == NULL )
-	{
-		fprintf(stderr, "Failed to allocate memory for sinewave\n");
-		exit(1);
-	}
-	wave->length = nsamples;
-
-	while ( sample < nsamples )
-	{
-		amplitude = sin(angle);
-		wave->wave[sample] = (tg_amplitude_t)(amplitude * scale);
-#if 0
-		fprintf(stderr, "%3d : %e : %e (%d)\n", sample, angle, amplitude, wave->wave[sample]);
-#endif
-		angle += angle_step;
-		sample++;
-	}
+	return (int)( sample_freq / sine_freq + 0.5 );
 }
+
+/* Calculate a sine wave of the given frequency
+*/
+int waveform(tg_sinewave_t *wave, double sine_freq)
+{
+	double angle, amplitude;
+	int nsamples = wavelen(sine_freq);
+	int i;
+
+#if HOST_TEST
+	fprintf(stderr, "Waveform for %fHz ; nsamples = %d\n", sine_freq, nsamples);
+#endif
+
+	if ( wave->length == nsamples )
+	{
+		for ( i = 0; i < nsamples; i++ )
+		{
+			angle = ((double)i * 2 * pi ) / (double)nsamples;
+			amplitude = sin(angle);
+			wave->wave[i] = (tg_amplitude_t)(amplitude * scale);
+		}
+	}
+	else
+	{
+#if HOST_TEST
+		fprintf(stderr, "wave->length is incorrect: %d\n", wave->length);
+#endif
+		return -1;
+	}
+	return 0;
+}
+
+#if HOST_TEST
+
+#define N_TEST	3
+
+double wfreq[N_TEST] =
+{	4186.01,	/* C8 */
+	2093.005,	/* C7 */
+	1046.5025	/* C6 */
+};
+
+tg_amplitude_t buffer[6000];
+
+int main(int argc, char **argv)
+{
+	tg_sinewave_t wave;
+	int t;
+	wave.wave = buffer;
+
+	for ( t = 0; t < N_TEST; t++ )
+	{
+		wave.length = wavelen(wfreq[t]);
+
+		if ( waveform(&wave, wfreq[t]) == 0 )
+		{
+			int i;
+			for ( i = 0; i < wave.length; i++ )
+			{
+				fprintf(stderr, "%5d = %d\n", i, wave.wave[i]);
+			}
+		}
+	}
+	return 0;
+}
+#endif
