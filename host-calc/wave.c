0abeae24df6331b4a9158730e557887f9ac25578
David Haworth <dh@thelancashireman.org>
2018-11-17 17:15:01 +0100

    Add file header

--- host-calc/wave.c
+++ host-calc/wave.c
@@ -1,3 +1,22 @@
+/*	wave.c - waveform generator to run on host PC (for experimentation)
+ *
+ *	Copyright 2018 David Haworth
+ *
+ *	This file is part of SynthEffect.
+ *
+ *	SynthEffect is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	SynthEffect is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with SynthEffect.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
