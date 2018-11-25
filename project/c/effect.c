90d89e940191bf4c38ffd6a5b061151c4b582418
David Haworth <dh@thelancashireman.org>
2018-11-25 10:32:40 +0100

    Add a root effect-stage structure and a function to process a sequence of effect stages.

---
+++ project/c/effect.c
@@ -0,0 +1,44 @@
+/* effect.c - control for an effect generator
+ *
+ * (c) 2018 David Haworth
+ *
+ * This file is part of SynthEffect.
+ *
+ * SynthEffect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SynthEffect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SynthEffect  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <kernel/h/dv-kconfig.h>
+#include <kernel/h/dv-types.h>
+#include <project/h/effect.h>
+
+effect_t *first = DV_NULL;
+
+/* This control structure controls a single, sequential processing sequence
+ * However, it should be possible to extend to a multiprocessing environment
+ * by having a "first" pointer per core and having some inter-core
+ * communication stages
+*/
+void effect_processor(void)
+{
+	for (;;)
+	{
+		effect_t *e = first;
+		dv_int64_t x = 0;
+
+		while ( e != DV_NULL )
+		{
+			x = e->func(e, x);
+			e = e->next;
+		}
+	}
+}
--- project/h/effect.h
+++ project/h/effect.h
@@ -2,48 +2,54 @@
  *
  * (c) 2018 David Haworth
  *
- * $(NAME) is free software: you can redistribute it and/or modify
+ * This file is part of SynthEffect.
+ *
+ * SynthEffect is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * $(NAME) is distributed in the hope that it will be useful,
+ * SynthEffect is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with $(NAME).  If not, see <http://www.gnu.org/licenses/>.
- *
- * $Id$
+ * along with SynthEffect  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef EFFECT_H
 #define EFFECT_H	1
 
+#include <kernel/h/dv-kconfig.h>
+#include <kernel/h/dv-types.h>
+
 /* The effects generator consists of a sequence of 2 or more single digital
- * transformations. Each transformation takes a configuration block and a sample as input,
- * and returns a transformed sample.
+ * transformations of an input signal.
  *
  * The first transformation in the sequence provides the input signal as a sample.
  * The final transformation outputs the final sample to whatever output medium is used.
+ * Thus the simplest case (2 stages) does no processing.
  *
- * A single effect module provides a configuration structure that contains an effect_t structure.
- * The effect_cfg pointer in the effect_t gives the address of the containing function.
+ * Each transformation stage is a function that takes a configuration block and a sample as input,
+ * and returns a transformed sample.
+ * A generator stage ignores the input sample and generates an output sample based on control input
+ * (e.g. key press/release events)
+ * 
+ * In the case of generation, it's possible that the process is recursive.
  *
- * The whole is managed using an effect_gen_t structure that contains the head of the
+ * The whole is managed using an effect_s structure that contains the head of the
  * list of transformations and some global configuration.
 */
-
 typedef struct effect_s effect_t;
-typedef int (*effect_function_t)(effect_t *, int);
+typedef dv_int64_t (*effectstage_t)(effect_t *, dv_int64_t);
 
 struct effect_s
 {
 	effect_t *next;
-	effect_function_t func;
+	effectstage_t func;
+	void *control;
 };
 
 extern effect_t *first;
-extern int samples_per_sec;
 
 #endif
