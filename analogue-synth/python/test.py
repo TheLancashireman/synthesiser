6fe900bef2b88394724d47468f1cc2499a8ad09d
David Haworth <dh@thelancashireman.org>
2021-04-28 23:28:54 +0200

    Add sequences

--- analogue-synth/python/test.py
+++ analogue-synth/python/test.py
@@ -34,6 +34,8 @@ def usage():
 	print('  g1   - set gate on')
 	print('  g0   - set gate off')
 	print('  t    - start tuning')
+	print('  s123 - play sequence 123')
+	print('  z    - start gate test: 1 Hz square wave')
 	print('  q    - quit')
 	return
 
@@ -80,11 +82,14 @@ def set_gate(v):
 	return
 
 def gate_on_off():
-	while True:
-		set_gate(0)
-		time.sleep(0.5)
-		set_gate(1)
-		time.sleep(0.5)
+	try:
+		while True:
+			set_gate(0)
+			time.sleep(0.5)
+			set_gate(1)
+			time.sleep(0.5)
+	except:
+		print('\nInterrupted')
 
 def start_tuning():
 	global ser
@@ -93,6 +98,84 @@ def start_tuning():
 	cmd = bytearray([0xb0+chan, 127, 127])
 	ser.write(cmd)
 	return
+
+def seqnote(n):
+	note_on(n)
+	time.sleep(0.3)
+	note_off(n)
+	time.sleep(0.1)
+
+def boogie(base):
+	seqnote(base)
+	seqnote(base+4)
+	seqnote(base+4+3)
+	seqnote(base+4+3+2)
+	seqnote(base+4+3+2+1)
+	seqnote(base+4+3+2)
+	seqnote(base+4+3)
+	seqnote(base+4)
+
+def seq0(base):
+	while True:
+		boogie(base)
+
+def seq1(base):
+	while True:
+		seqnote(base)
+		seqnote(base+4)
+		seqnote(base+7)
+		seqnote(base+9)
+		seqnote(base+12)
+		seqnote(base+9)
+		seqnote(base+7)
+		seqnote(base+4)
+		seqnote(base+7)
+		seqnote(base-5)
+		seqnote(base-1)
+		seqnote(base+2)
+		seqnote(base+7)
+		seqnote(base+4)
+		seqnote(base+2)
+		seqnote(base+4)
+		seqnote(base+9)
+		seqnote(base+4)
+		seqnote(base+9)
+		seqnote(base+7)
+		seqnote(base+5)
+		seqnote(base)
+		seqnote(base+5)
+		seqnote(base+7)
+		seqnote(base+9)
+		seqnote(base+7)
+		seqnote(base+16)
+		seqnote(base+14)
+		seqnote(base+12)
+		seqnote(base+11)
+		seqnote(base+9)
+		seqnote(base+7)
+
+def seq2(base):
+	while True:
+		boogie(base)
+		boogie(base)
+		boogie(base+5)
+		boogie(base)
+		boogie(base+7)
+		boogie(base)
+
+
+def play_sequence(n):
+	try:
+		if n == 0:
+			seq0(48)
+		elif n == 1:
+			seq1(36)
+		elif n == 2:
+			seq2(36)
+		else:
+			print('No sequence #' + n)
+	except:
+			print('\nInterrupted')
 
 lastnote = -1
 
@@ -113,37 +196,46 @@ for l in sys.stdin:
 			if len(n) > 0:
 				try:
 					v = int(n)
-					if v >= 0 and v <= 127:
-						lastnote = v
-						note_on(v)
-					else:
-						usage()
 				except:
+					v = -1
+				if v >= 0 and v <= 127:
+					lastnote = v
+					note_on(v)
+				else:
 					usage()
 			elif lastnote >= 0:
-					note_off(lastnote)
-					lastnote = -1
+				note_off(lastnote)
+				lastnote = -1
 		elif c == 'c' or c == 'f':
-#			try:
-			v = int(n)
+			try:
+				v = int(n)
+			except:
+				v = -1
 			if v >= 0 and v <= 255:
 				set_dac(c, v)
 			else:
 				usage()
-#			except:
-#				usage()
 		elif c == 'g':
 			if len(n) > 0:
 				try:
 					v = int(n)
-					if v >= 0 and v <= 127:
-						set_gate(v)
-					else:
-						usage()
 				except:
+					v = -1
+				if v >= 0 and v <= 1:
+					set_gate(v)
+				else:
 					usage()
 			else:
 				usage()
+		elif c == 's':
+			if len(n) > 0:
+				try:
+					v = int(n)
+				except:
+					v = -1
+			else:
+				v = 0
+			play_sequence(v)
 		elif c == 't':
 			start_tuning()
 		elif c == 'z':
